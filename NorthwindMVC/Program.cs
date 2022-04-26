2eb6dc7cffebd64538b64e397fc450a95d9394bf
Recaedae <lemour@live.co.uk>
2022-04-26 08:30:05 +0200

    Configured NorthwindMVC to allow connection to the NorthwindService API.

--- NorthwindMVC/Program.cs
+++ NorthwindMVC/Program.cs
@@ -21,6 +21,7 @@ namespace NorthwindMVC
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://localhost:5000", "https://localhost:5002");
                 });
     }
 }
